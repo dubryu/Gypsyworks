aa777ea2ad4721930bc37a7eb191a87efc3708a9
dubryu <dubryu@users.noreply.github.com>
2021-12-22 08:19:11 +0000

    deploy: c29258060e515276082c12f5b46a0e212c5dc0c8

---
+++ _nuxt/static/1640161149/catalog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgypsyworks\u002F_nuxt\u002Fstatic\u002F1640161149",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcatalog",config:{_app:{basePath:"\u002Fgypsyworks\u002F",assetsPath:"\u002Fgypsyworks\u002F_nuxt\u002F",cdnURL:a}}}}(null));
