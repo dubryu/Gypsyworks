9e308993db6f954961b82877fb6fe7f410e460a9
dubryu <dubryu@users.noreply.github.com>
2021-12-29 17:32:20 +0000

    deploy: ac4a03e1f249f818f9225354364f46f6b26afa39

--- _nuxt/static/1640798539/animation/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgypsyworks\u002F_nuxt\u002Fstatic\u002F1640798539",layout:"default",error:a,serverRendered:true,routePath:"\u002Fanimation",config:{_app:{basePath:"\u002Fgypsyworks\u002F",assetsPath:"\u002Fgypsyworks\u002F_nuxt\u002F",cdnURL:a}}}}(null));
