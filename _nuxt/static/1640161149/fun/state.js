76cb1d724fa9e89a11ede6c2e61569f51e1e1c3e
dubryu <dubryu@users.noreply.github.com>
2021-12-29 17:22:22 +0000

    deploy: 6d3cd72150a497f8c06f8ab47462c5ca28fe80e8

--- _nuxt/static/1640161149/fun/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgypsyworks\u002F_nuxt\u002Fstatic\u002F1640161149",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffun",config:{_app:{basePath:"\u002Fgypsyworks\u002F",assetsPath:"\u002Fgypsyworks\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1640798539/animation/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/animation", {data:[{}],fetch:{},mutations:void 0});
