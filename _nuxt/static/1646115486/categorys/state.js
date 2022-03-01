2ea1a80ed9cb08bd8246e127f86305371aeab137
dubryu <dubryu@users.noreply.github.com>
2022-03-01 06:22:57 +0000

    deploy: f1a351d4a81356db6b564f40db67f69311f90a45

--- _nuxt/static/1646115486/categorys/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgypsyworks\u002F_nuxt\u002Fstatic\u002F1646115486",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Fcategorys",config:{_app:{basePath:"\u002Fgypsyworks\u002F",assetsPath:"\u002Fgypsyworks\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1646115486/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcatalog","\u002Fcatalog_parallax","\u002Ffun","\u002Fcategorys"]})
