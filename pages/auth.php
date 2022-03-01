3eda780103760a70e99129f71cdb38619ccc8ff3
ryusuke katayama <>
2022-03-01 20:57:09 +0900

    auth.php

--- pages/auth.php
+++ pages/auth.php
@@ -1,8 +1,8 @@
 <?php
 
   define("OAUTH2_SITE", 'https://api.shop-pro.jp');
-  define("OAUTH2_CLIENT_ID",'xxx'); // xxxにクライアントIDを入力
-  define("OAUTH2_CLIENT_SECRET", 'yyy'); // yyyにクライアントシークレットを入力
+  define("OAUTH2_CLIENT_ID",'0126ddc5a03b7e0d9de14934e26012ae0132a56e976225e7221f7af07be9e313'); // xxxにクライアントIDを入力
+  define("OAUTH2_CLIENT_SECRET", '846055ac700f3d7c98d87a42e1875d4580b117d7e1ec35cd1f7e9822b1d45f20'); // yyyにクライアントシークレットを入力
   define("OAUTH2_REDIRECT_URI", 'http://example.com/auth.php'); // リダイレクトURLを入力
 
   $code = $_GET['code'];
