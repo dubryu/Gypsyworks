b8be53c3a56d2bae4da15b727f9b755531d2ecfa
ryusuke katayama <>
2022-03-02 00:27:25 +0900

    retry

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,9 +79,9 @@ export default {
     },
     strategies: {
       colorme: {
-        client_id: "3206fb3506571dc4a400dab19b8319b5ce1fdb5f1bf069e5a943a521d60a6462",
-        client_secret: "2a52dff122e7ead048d0ff87b4687123163985ce6ba0706bd2e7e8f4fedfc3b2",
-        code: "a99755df020d16d4b1fea8e9e2ce0eb724663aaf119dcc42618b6e37f1a2876c",
+        client_id: 'cf1b5a845474eb77cef50e84089c962d7e7da35817d9add4c9e658966c7fad54',
+        client_secret: 'fb2b45126fb414ad63f7eebc253a142a509a796cdf8adad056b08cd0cd43b7b5',
+        code: 'f2daf4ad9b7bcfdbd9f6b0eceff550be280c8e63174e5a5e9817dee6fca5f530',
         grant_type: 'authorization_code',
         // redirect_uri: 'https://dubryu.github.io/gypsyworks/',
         scope: ['read_products'],
