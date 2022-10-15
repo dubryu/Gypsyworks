6e3a2d29b7cfa686bce082d32130862a8087c2d4
dubryu <dubryu@users.noreply.github.com>
2022-10-15 13:15:39 +0000

    deploy: feaaeba22750c021fbb863c00a27abeb4cd293a5

--- _nuxt/2841f38.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{350:function(n,o,e){"use strict";e.r(o);var t=e(47),component=Object(t.a)({},(function(){var n=this,o=n.$createElement;return(n._self._c||o)("h1",[n._v("boooooo")])}),[],!1,null,null,null);o.default=component.exports}}]);
