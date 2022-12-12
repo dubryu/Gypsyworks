bf74caea848ec5211048f61fcd30828ff4bc78f7
dubryu <dubryu@users.noreply.github.com>
2022-12-12 22:19:24 +0000

    deploy: df41270b8031070201494a35a5bc7056c910bad4

--- _nuxt/1bebf5d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{425:function(o,n,t){"use strict";t.r(n);var e=t(52),component=Object(e.a)({},(function(){return(0,this._self._c)("h1",[this._v("boooooo")])}),[],!1,null,null,null);n.default=component.exports}}]);
